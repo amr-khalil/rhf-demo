1ed3a0f17affad9db0d40d402160ee5bd6429a4f
Amr Khalil <amr.khalil@haufe-lexware.net>
2025-06-17 23:54:07 +0200

    add dynamic form

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,24 +1,50 @@
 import { z } from "zod";
 
-export const userSchema = z.object({
-  name: z.string().min(1, { message: "Required" }),
-  email: z
-    .string()
-    .min(1, { message: "Email is required" })
-    .email({ message: "Invalid email address" }),
-  state: z.array(z.string()).min(1).max(2),
-  languagesSpoken: z.array(z.string()),
-  gender: z.string().min(1, { message: "Required" }),
-  skills: z.array(z.string()).max(2),
-  registrationDateAndTime: z.date(),
-  formerEmploymentPeriod: z.array(z.date()).min(2).max(2),
-  salaryRange: z.array(z.number()).min(2).max(2),
-  isTeacher: z.boolean(),
-});
+export const userSchema = z
+  .intersection(
+    z.object({
+      name: z.string().min(1, { message: "Required" }),
+      email: z
+        .string()
+        .min(1, { message: "Email is required" })
+        .email({ message: "Invalid email address" }),
+      state: z.array(z.string()).min(1).max(2),
+      languagesSpoken: z.array(z.string()),
+      gender: z.string().min(1, { message: "Required" }),
+      skills: z.array(z.string()).max(2),
+      registrationDateAndTime: z.date(),
+      formerEmploymentPeriod: z.array(z.date()).min(2).max(2),
+      salaryRange: z.array(z.number()).min(2).max(2),
+      isTeacher: z.boolean(),
+    }),
+    z.discriminatedUnion("variant", [
+      z.object({
+        variant: z.literal("create"),
+      }),
+      z.object({
+        variant: z.literal("edit"),
+        id: z.string().min(1),
+      }),
+    ]),
+  )
+  .and(
+    z.union([
+      z.object({ isTeacher: z.literal(false) }),
+      z.object({
+        isTeacher: z.literal(true),
+        students: z.array(
+          z.object({
+            name: z.string().min(4, { message: "Required" }),
+          }),
+        ),
+      }),
+    ]),
+  );
 
 export type UserSchemaType = z.infer<typeof userSchema>;
 
 export const defaultValues: UserSchemaType = {
+  variant: "create",
   name: "",
   email: "",
   state: [],
@@ -28,5 +54,5 @@ export const defaultValues: UserSchemaType = {
   registrationDateAndTime: new Date(),
   formerEmploymentPeriod: [new Date(), new Date()],
   salaryRange: [0, 3000],
-  isTeacher: true,
+  isTeacher: false,
 };
--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,8 +1,7 @@
-import { useFormContext } from "react-hook-form";
-import TextField from "@mui/material/TextField";
-import { Stack, Typography } from "@mui/material";
+import { useFieldArray, useFormContext, useWatch } from "react-hook-form";
+import { Button, Container, Stack, Typography } from "@mui/material";
 import RHFAutocomplete from "../../components/RHFAutocomplete";
-import type { UserSchemaType } from "../../types/schema";
+import { defaultValues, type UserSchemaType } from "../../types/schema";
 import {
   useGenders,
   useLanguages,
@@ -17,15 +16,44 @@ import RHFDateRangePicker from "../../components/RHFDateRangePicker";
 import RHFSlider from "../../components/RHFSlider";
 import RHFSwitch from "../../components/RHFSwitch";
 import RHFTexfField from "../../components/RHFTexfField";
+import { Fragment, useEffect } from "react";
 
 function Users() {
   const statesQuery = useStates();
   const languagesQuery = useLanguages();
   const genderQuery = useGenders();
   const skillsQuery = useSkills();
+
+  const { watch, control, unregister, reset } =
+    useFormContext<UserSchemaType>();
+
+  useEffect(() => {
+    const sub = watch((value) => {
+      console.log("Form values changed:", value);
+    });
+    return () => sub.unsubscribe();
+  }, [watch]);
+
+  const isTeacher = useWatch({ control, name: "isTeacher" });
+  const { append, remove, replace, fields } = useFieldArray({
+    control,
+    name: "students",
+  });
+
+  useEffect(() => {
+    if (!isTeacher) {
+      replace([]);
+    }
+    unregister("students");
+  }, [isTeacher, replace, unregister]);
+
+  const handleReset = () => {
+    reset(defaultValues);
+  };
+
   return (
-    <>
-      <Stack gap={2} sx={{ width: "300px" }}>
+    <Container maxWidth="sm" component={"form"}>
+      <Stack gap={2}>
         <RHFTexfField<UserSchemaType> name="name" label="Name" />
         <RHFTexfField<UserSchemaType> name="email" label="Email" />
         <RHFAutocomplete<UserSchemaType>
@@ -58,8 +86,34 @@ function Users() {
           name="isTeacher"
           label="Are you a teacher?"
         />
+        {isTeacher && (
+          <Button onClick={() => append({ name: "" })} type="button">
+            Add New Student
+          </Button>
+        )}
+        {fields.map((field, index) => (
+          <Fragment key={field.id}>
+            <RHFTexfField<UserSchemaType>
+              name={`students.${index}.name`}
+              label="Name"
+              key={field.id}
+            />
+
+            <Button color="error" onClick={() => remove(index)} type="button">
+              Remove
+            </Button>
+          </Fragment>
+        ))}
+        <Stack direction="row" justifyContent="space-between">
+          <Button type="reset" variant="outlined" onClick={handleReset}>
+            Reset
+          </Button>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </Stack>
       </Stack>
-    </>
+    </Container>
   );
 }
 
